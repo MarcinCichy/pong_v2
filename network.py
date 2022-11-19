f23da868f466f01ee5e9aaabc894990deccd1bae
Marcin Cichy <marcin-cichy@wp.pl>
2022-11-19 17:42:03 +0100

    Added usage of JSON

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import socket
+import json
 
 PORT = 65432
 BUFFER = 2048
@@ -9,25 +10,28 @@ class Network:
 
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = HOST # For this to work on your machine this must be equal to the ipv4 address of the machine running the server
-                                    # You can find this address by typing ipconfig in CMD and copying the ipv4 address. Again this must be the servers
-                                    # ipv4 address. This feild will be the same for all your clients.
+        self.host = HOST
         self.port = PORT
         self.addr = (self.host, self.port)
         self.id = self.connect()
-
+        print(self.id)
+        
+        
     def connect(self):
         self.client.connect(self.addr)
-        return self.client.recv(BUFFER).decode()
+        data = self.client.recv(BUFFER)
+        data_decode = json.loads(data)  #  czy to jest konieczne, i tak dane przesyłane sa przez JSON dalej
+        return data_decode
 
+
+    
     def send(self, data):
         """
-        :param data: str
-        :return: str
+        data; JSON
         """
         try:
-            self.client.send(str.encode(data))
-            reply = self.client.recv(2048).decode()
+            self.client.send(data)  # tutaj nie dałem encodowania, bo otrzymujemy dane w postaci JSONa i wysyałamy je dalej
+            reply = self.client.recv(BUFFER)
             return reply
         except socket.error as e:
             return str(e)
