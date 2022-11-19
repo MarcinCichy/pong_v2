097d9366ec5843c73caf661bb1c060170d3348bc
Marcin Cichy <marcin-cichy@wp.pl>
2022-11-19 09:47:10 +0100

    First version of network module

---
+++ network.py
@@ -0,0 +1,33 @@
+import socket
+
+PORT = 65432
+BUFFER = 2048
+HOST = "127.0.0.1"
+
+
+class Network:
+
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = HOST # For this to work on your machine this must be equal to the ipv4 address of the machine running the server
+                                    # You can find this address by typing ipconfig in CMD and copying the ipv4 address. Again this must be the servers
+                                    # ipv4 address. This feild will be the same for all your clients.
+        self.port = PORT
+        self.addr = (self.host, self.port)
+        self.id = self.connect()
+
+    def connect(self):
+        self.client.connect(self.addr)
+        return self.client.recv(BUFFER).decode()
+
+    def send(self, data):
+        """
+        :param data: str
+        :return: str
+        """
+        try:
+            self.client.send(str.encode(data))
+            reply = self.client.recv(2048).decode()
+            return reply
+        except socket.error as e:
+            return str(e)
